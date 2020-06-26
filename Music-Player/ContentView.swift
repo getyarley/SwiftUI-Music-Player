debb8e4920d6d12ea79f36672fb7947d3a213a0d
Jeremy Yarley <jeremy.yarley@gmail.com>
2020-06-25 21:04:44 -0400

    Initial Commit

---
+++ Music-Player/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music-Player
+//
+//  Created by Jeremy Yarley on 6/25/20.
+//  Copyright © 2020 Jeremy Yarley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
